ed36d9f7b1bab8d91dd7fd599a3a6caa73365798
Nurlalailinur <lailinurlaili556@gmail.com>
2023-06-11 11:56:57 +0800

    laravel

---
+++ README.md
@@ -0,0 +1,1 @@
+# ib-fswd-laravel
---
+++ index.blade.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Tabel User</title>
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+
+		th, td {
+			text-align: left;
+			padding: 8px;
+		}
+
+		tr:nth-child(even) {
+			background-color: #f2f2f2
+		}
+
+		th {
+			background-color: #4CAF50;
+			color: white;
+		}
+
+		td img {
+			max-width: 50px;
+			border-radius: 50%;
+		}
+
+		button {
+			background-color: #4CAF50;
+			border: none;
+			color: white;
+			padding: 8px 16px;
+			border-radius: 4px;
+			cursor: pointer;
+			transition: background-color 0.3s;
+		}
+
+		button:hover {
+			background-color: #3e8e41;
+		}
+	</style>
+    
+        
+    
+	
+<body>
+</head>
+
+
+
+  
+
+	
+    <div class="container">
+        <div class="table-wrapper">
+          <div class="table-title">
+            <div class="row">
+              <div class="col-sm-6">
+                <h2>Data <b>Pengguna</b></h2>
+                
+            </div>
+          </div>
+	<table>
+		<thead>
+            <td>
+				
+                <a href="tambah _pengguna.html" class="action-btn tambah-btn"><button>ADD Pengguna</button</a>
+                </td>   
+			<tr>
+        <th>No</th>
+				<th>Aksi</th>
+				<th>Avatar</th>
+				<th>Nama</th>
+				<th>Email</th>
+				<th>Phone</th>
+				<th>Role</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+        <td>1</td>
+				<td>
+					<a href="edit_user.html" class="action-btn edit-btn"><button>Edits</button</a>
+                    <a href="delete_user.html" class="action-btn edit-btn"><button>Hapus</button</a>
+				</td>
+				<td><img src="https://cdn-icons-png.flaticon.com/128/4140/4140047.png" alt="Avatar"></td>
+				<td>Nurlaili</td>
+				<td>nurlaily@mail.com</td>
+				<td>123-456-7890</td>
+				<td>Admin</td>
+			</tr>
+			<tr>
+        <td>2</td>
+				<td>
+					<a href="edit_user.html" class="action-btn edit-btn"><button>Edits</button</a>
+                    <a href="delete_user.html" class="action-btn edit-btn"><button>Hapus</button</a>
+				</td>
+				<td><img src="https://cdn-icons-png.flaticon.com/128/4140/4140047.png" alt="Avatar"></td>
+				<td>Jani Susilawati</td>
+				<td>jani.susilawati@mail.com</td>
+				<td>555-555-5555</td>
+				<td>User</td>
+			</tr>
+			<tr>
+                <td>3</td>
+                        <td>
+                            <a href="edit_user.html" class="action-btn edit-btn"><button>Edits</button</a>
+                            <a href="delete_user.html" class="action-btn edit-btn"><button>Hapus</button</a>
+                        </td>
+                        <td><img src="https://cdn-icons-png.flaticon.com/128/4140/4140047.png" alt="Avatar"></td>
+                        <td>laily</td>
+                        <td>laily_nur@mail.com</td>
+                        <td>765-555-9875</td>
+                        <td>User</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                                <td>
+                                    <a href="edit_user.html" class="action-btn edit-btn"><button>Edits</button</a>
+                                    <a href="delete_user.html" class="action-btn edit-btn"><button>Hapus</button</a>
+                                </td>
+                                <td><img src="https://cdn-icons-png.flaticon.com/128/4140/4140047.png" alt="Avatar"></td>
+                                <td>Fira Wartini</td>
+                                <td>frawartini@mail.com</td>
+                                <td>781-999-346</td>
+                                <td>User</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                        <td>
+                                            <a href="edit_user.html" class="action-btn edit-btn"><button>Edits</button</a>
+                                            <a href="delete_user.html" class="action-btn edit-btn"><button>Hapus</button</a>
+                                        </td>
+                                        <td><img src="https://cdn-icons-png.flaticon.com/128/4140/4140047.png" alt="Avatar"></td>
+                                        <td>Maria salim</td>
+                                        <td>mariasalim@mail.com</td>
+                                        <td>888-561-0086</td>
+                                        <td>User</td>
+                                    </tr>
+    </td>
+    </tr>
+                                    
+                                    
+		</tbody>
+	</table>
+   
+</tbody>
+</html>
